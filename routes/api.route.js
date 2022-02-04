5335870dd2a928edf14a946da75d84f21d653968
Gyan Ankit <g.gyanankit@gmail.com>
2022-02-04 11:39:30 +0530

    routes updated

--- routes/api.route.js
+++ routes/api.route.js
@@ -3,9 +3,11 @@ const parking = require('../controllers/parking.controller')
 
 const router = express.Router();
 
-router.post('/booking', parking.booking);
 router.get('/available-slots', parking.availableSlot);
-router.get('/occupied-slots', parking.availableSlot);
+router.get('/occupied-slots', parking.occupiedSlot);
 router.get('/registered-users', parking.registeredUsers);
+router.delete('/booking-over', parking.bookingDelete);
+router.put('/booking-update', parking.bookingUpdate);
+router.post('/booking', parking.booking);
 
 module.exports = router;
