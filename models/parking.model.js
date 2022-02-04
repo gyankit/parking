43e3f0889a4a02214550f028bb30cafe4a5ac844
Gyan Ankit <g.gyanankit@gmail.com>
2022-02-04 11:40:02 +0530

    models updated

--- models/parking.model.js
+++ models/parking.model.js
@@ -1,12 +1,18 @@
 const mongoose = require('mongoose');
 
 const parkingSchema = new mongoose.Schema({
-    userName: { type: String, required: true },
-    vehicleNo: { type: String, required: true },
-    reserved: { type: Boolean, required: true },
-    comment: { type: String, required: false },
-    slotNo: { type: Number, required: true },
-}, { timestamps: true });
+    user_name: { type: String, required: true },
+    vehicle_number: { type: String, required: true },
+    parking_number: { type: Number, required: true },
+    special_booking: { type: Boolean, required: true },
+    parking_confirmation: { type: Boolean, required: true },
+    comment_if_special_booking: { type: String, required: false },
+}, {
+    timestamps: {
+        createdAt: 'booking_time',
+        updatedAt: 'booking_confirm_time'
+    }
+});
 
 const Parking = mongoose.model('Parking', parkingSchema);
 
